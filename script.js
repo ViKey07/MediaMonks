4c4e2c1b62beffa272a5a691d1d3abd785d32648
TECH I.S <techi.s.@TECHs-MacBook-Air-9.local>
2023-02-09 00:37:43 +0530

    Done with project

--- script.js
+++ script.js
@@ -1,79 +1,84 @@
 window.onload = function() {
-    var nextArrow = document.getElementById('next'),
+    let nextArrow = document.getElementById('next'),
     backArrow = document.getElementById('back'),
     backImg = document.getElementById('back-img'),
     rotation = document.getElementById('rotation'),
-    newPosition = 0,
+    newLocations = 0,
 	currentId = 0,
 	prevId;
 
-    var coordinatesArray = [ 0, 125, 225, 350, 495, 625, 780, 900, 950, 1060 ];
-	var lengthUnit = "vh";
+    let locations = [0, 125, 225, 350, 495, 625, 780, 900, 950, 1060];
+	let lengthUnit = "vh";
 
-    function getCoordinates(coordinatesArray, pageId) {
-		return "-" + coordinatesArray[pageId];
+    function getCoordinates(locations, pageId) {
+		return "-" + locations[pageId];
 	};
 
     nextArrow.addEventListener( "click", function(){
+		checkArrowVisibility()
 		currentId ++;
-		var nextPageCoordinates = getCoordinates(coordinatesArray, currentId);
+		let nextPageCoordinates = getCoordinates(locations, currentId);
 		renderPage(nextPageCoordinates);
 	});
 
     backArrow.addEventListener( "click", function(){
+		checkArrowVisibility()
 		currentId --;
-		var nextPageCoordinates = getCoordinates(coordinatesArray, currentId);
+		let nextPageCoordinates = getCoordinates(locations, currentId);
 		renderPage(nextPageCoordinates);
 	});
 
     rotation.addEventListener("click", function(event){
-		var coordinates = getCoordinates(coordinatesArray, event.target.id);
+		console.log(newLocations);
+
+		let coordinates = getCoordinates(locations, event.target.id);
 		currentId = event.target.id;
 		renderPage(coordinates);
 	});
 
     function checkArrowVisibility() {
 		if (currentId == 9) {
+		console.log('if');
+
 			nextArrow.style.visibility = "hidden";
-		} else if (currentId == 0) {
+		} else if (newLocations == 0) {
+		console.log('elif');
+
 			backArrow.style.visibility = "hidden";
 		} else {
+		console.log('else');
+
 			nextArrow.style.visibility = "visible";
 			backArrow.style.visibility = "visible";
 		}
 	}
 
     function renderPage(coordinates) {
-		// set new bg position
-		newPosition = coordinates;
-		backImg.style["transform"] = "translate(" + newPosition + lengthUnit + ", 0)";
+		newLocations = coordinates;
+		backImg.style["transform"] = "translate(" + newLocations + lengthUnit + ", 0)";
 
-		// hide previous content
 		if (prevId != null) {
-			var prevArticleId = "article--" + prevId,
-					prevArticle = document.getElementById(prevArticleId),
+			let prevSecId = "sec-" + prevId,
+					prevSec = document.getElementById(prevSecId),
 					prevNavButton = document.getElementById(prevId);
 
 			if (prevId != 9) {
-				prevArticle.style.opacity = 0;
+				prevSec.style.opacity = 0;
 			}
 			prevNavButton.className = "";
 		}
 
-		// display current page on navigation
-		var newNavButton = document.getElementById(currentId);
-		newNavButton.className = "listitem--clicked"; 
+		let newNavButton = document.getElementById(currentId);
+		newNavButton.className = "li-click"; 
 
-		// display current page contents
 		setTimeout(function(){
-			var articleId = "article--" + currentId, 
-					currentArticle = document.getElementById(articleId);
+			let secId = "sec-" + currentId, 
+					currentSec = document.getElementById(secId);
 
-			currentArticle.style.opacity = 1;
+			currentSec.style.opacity = 1;
 		}, 1500);
 
 		checkArrowVisibility();
-		// set previous page id to the current one 
 		prevId = currentId;
 	}
 
